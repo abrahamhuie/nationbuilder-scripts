118d4b5049db03f179066da86dd5d3397fa111e6
Abe <31999486+abrahamhuie@users.noreply.github.com>
2019-03-04 22:44:34 -0800

    Update parse_indexed_events.rb
    Fixed typos. Increased event limit to 100. Accounted for pagination.

--- parse_indexed_events.rb
+++ parse_indexed_events.rb
@@ -1,24 +1,27 @@
 require 'nationbuilder'
 require 'pp'
-require 'json'
 
 nation_slug = '[nation_slug]'
 api_token = '[api_token]'
 client = NationBuilder::Client.new(nation_slug, api_token, retries: 8)
+response = client.call(:events, :index, site_slug: 'site_slug', limit: 100)
+paginated = NationBuilder::Paginator.new(client, response)
+page1 = paginated
+page2 = page1.next
+page3 = page2.next
 
-#Paginated index of NB Events
-index = client.call(:events, :index, site_slug: 'site_slug')
-
-#Array of events
-events = index['results']
+#Events arrays, limit 100
+events1 = response['results']
+events2 = page1.body['results']
+events3 = page2.body['results']
 
 #Pretty print array of events
-pp events
+pp events1
 
 #First event in the results array
-index['results'].first
+events1.first
 
 #Iterate through the array of events and print the ids in the array
-events.each do |event|
+events1.each do |event|
 puts event['id']
 end
