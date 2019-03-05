d1bca061e481222abf611a61bb8ffecf2e1c8435
Abe <31999486+abrahamhuie@users.noreply.github.com>
2019-03-04 22:53:13 -0800

    Update destroy_events.rb

--- destroy_events.rb
+++ destroy_events.rb
@@ -1,6 +1,6 @@
 require 'nationbuilder'
 
-nation_slug = '[nation_slug]]'
+nation_slug = '[nation_slug]'
 api_token = '[api_token]'
 client = NationBuilder::Client.new(nation_slug, api_token, retries: 8)
 
