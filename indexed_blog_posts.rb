1a370b614e88df91cc252b452d483ac9d1792f82
Abe <31999486+abrahamhuie@users.noreply.github.com>
2019-04-19 10:51:19 -0700

    Refactored

---
+++ indexed_blog_posts.rb
@@ -0,0 +1,19 @@
+require 'nationbuilder'
+require 'pp'
+
+nation_slug = '[nation_slug]'
+api_token = '[api_token]'
+client = NationBuilder::Client.new(nation_slug, api_token, retries: 8)
+
+response = client.call(:blog_posts, :index, site_slug: #, id: #, limit: 100)
+paginated = NationBuilder::Paginator.new(client, response)
+
+page1 = paginated
+
+blogs1 = page1['results']
+#Array of blog posts
+
+blogs1.each do |blog|
+  puts blog['id']
+end
+#Iterate through the array of blog posts and print the ids in the array
