6c40341463092724ddd86134f62fa5186de6fda0
Abe <31999486+abrahamhuie@users.noreply.github.com>
2019-05-28 13:37:09 -0700

    Update update_signup.rb
    Refactored to pass id as a key in the params hash.

--- update_signup.rb
+++ update_signup.rb
@@ -5,10 +5,10 @@ api_token = '[api_token]'
 client = NationBuilder::Client.new(nation_slug, api_token, retries: 8)
 
 params = {
+  id: 286740,
   person: {
     last_name: "Pina",
-    first_name: "Jimmy",
-    id: 286740
+    first_name: "Jimmy"
   }
 }
 
