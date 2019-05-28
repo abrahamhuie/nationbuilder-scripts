bf7ba6edd2ce52bb17e3cfffc094b4c097d167ac
Abe <31999486+abrahamhuie@users.noreply.github.com>
2019-05-28 13:50:25 -0700

    Update destroy_blog_pages.rb

--- destroy_blog_pages.rb
+++ destroy_blog_pages.rb
@@ -7,7 +7,7 @@ client = NationBuilder::Client.new(nation_slug, api_token, retries: 8)
 spam_pages = []
 
 spam_pages.each do |id|
-	client.call(:blog_posts, :destroy, site_slug: '[site_slug]', blog_id: #, blog_post_id: #)
+	client.call(:blog_posts, :destroy, site_slug: '[site_slug]', blog_id: [blog_id], blog_post_id: [blog_post_id])
 	puts "Deleted blog post:#{id}"
 	sleep(6.5)
 end
