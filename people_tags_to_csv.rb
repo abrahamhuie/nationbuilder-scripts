e5b7373c1c758655da231b48691c6ed1a5684c9c
Abe <31999486+abrahamhuie@users.noreply.github.com>
2019-09-11 09:57:15 -0700

    Update people_tags_to_csv.rb
    refactored to remove extraneous iterator.

--- people_tags_to_csv.rb
+++ people_tags_to_csv.rb
@@ -9,29 +9,29 @@ puts "Connected to #{nation_slug}."
 
 response = client.call(:people_tags, :index, limit: 100)
 paginated = NationBuilder::Paginator.new(client, response)
-#initiate pagination
 
 page1 = paginated
-#pagination results
+page2 = page1.next
+page3 = page2.next
+page4 = page3.next
+page5 = page4.next
+page6 = page5.next
 
-tags_array1 = page1.body["results"]
-#push pagination results into an array of tags
+tags_array = []
 
-tags_1 = []
-
-puts "Processing..."
-
-tags_array1.each {|x| tags_1.push(x["name"]) }
-sleep(5)
-#pushes tag names into array
+page1.body["results"].each { |x| tags_array << x["name"]}
+page2.body["results"].each { |x| tags_array << x["name"]}
+page3.body["results"].each { |x| tags_array << x["name"]}
+page4.body["results"].each { |x| tags_array << x["name"]}
+page5.body["results"].each { |x| tags_array << x["name"]}
+page6.body["results"].each { |x| tags_array << x["name"]}
 
 puts "Getting tags from #{nation_slug}, adding them to CSV #{nation_slug}_people_tags.csv."
 
 CSV.open("#{nation_slug}_people_tags.csv", "w") do |x|
-  tags_1.each do |y|
+  tags_array.each do |y|
     x << [y]
   end
 end
-#adds tags to csv
 
 puts "CSV ready."
