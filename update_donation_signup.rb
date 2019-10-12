189cb8abe77f1a283686bb2e0e0d99ae73211ba0
Abe <31999486+abrahamhuie@users.noreply.github.com>
2019-10-11 17:42:15 -0700

    Update update_donation_signup.rb

--- update_donation_signup.rb
+++ update_donation_signup.rb
@@ -7,9 +7,8 @@ client = NationBuilder::Client.new(nation_slug, api_token, retries: 8)
 params = {
 	"id": #,
 	"donation": {
-	      "donor_id": #,
+	      "donor_id": #
       	}
-}
 }
 
 client.call(:donations, :update, params)
